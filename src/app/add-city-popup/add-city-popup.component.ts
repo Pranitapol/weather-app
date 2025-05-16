5ff873f0d1df245d172cffc2b26d55e6ef90c3a7
unknown <polpranita26@gmail.com>
2025-05-16 12:31:26 +0530

    weather info by city

--- src/app/add-city-popup/add-city-popup.component.ts
+++ src/app/add-city-popup/add-city-popup.component.ts
@@ -5,16 +5,28 @@ import {MatFormFieldModule} from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { WeatherApiService } from '../weather-api.service';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule, NgIf } from '@angular/common';
+// import * as from '../../../assets'
 @Component({
   selector: 'app-add-city-popup',
   standalone: true,
-  imports: [MatDialogModule,MatFormFieldModule,ReactiveFormsModule,MatIconModule,MatInputModule],
+  imports: [MatDialogModule,MatFormFieldModule,ReactiveFormsModule,MatIconModule,MatInputModule,MatButtonModule,
+    NgIf,CommonModule
+  ],
   templateUrl: './add-city-popup.component.html',
   styleUrl: './add-city-popup.component.scss'
 })
 export class AddCityPopupComponent implements OnInit{
   result!: Object;
-  error: any;
+  error: any='';
+  weatherDesc:string='';
+  cityName:string='';
+  temp:any;
+  localDate: any;
+  weatherSrc:string='';
+  dataToPass:any;
+
   constructor(private dialogref:MatDialogRef<AddCityPopupComponent>,@Inject(MAT_DIALOG_DATA)public data:any,
   private service:WeatherApiService){}
 
@@ -32,15 +44,63 @@ export class AddCityPopupComponent implements OnInit{
     console.log('in search',this.inputValue)
     this.service.getData(this.inputValue).subscribe(
     { 
-      next: (res)=>{
+      next: (res:any)=>{
+        this.error=null
         this.result=res
-        console.log('in response',res)
+        this.weatherDesc=res.weather[0].main;
+        this.cityName=res.name;
+        this.temp=Math.floor(res.main.temp);
+        this.localDate = new Date(res.dt * 1000);
+        // const localdate = new Date(utcDate.getTime() + res.timezone * 1000); 
+        // this.localDate = localdate;
+
+        this.getWeatherIcon()
+      
       },
       error:(err)=>{
+        this.result='';
+        if(err){
         this.error=err.error.message
-        console.log('in response',this.error)
+        console.log(this.error,err)
+        }
       }
     }
     )
+  }
+
+  getWeatherIcon(){
+    switch(this.weatherDesc){
+      case 'Clouds':
+        this.weatherSrc= 'assets/cloudy.svg';
+        break;
+        case 'Rain':
+           this.weatherSrc='assets/rainy-6.svg';
+           break;
+        case 'Snow':
+          this.weatherSrc= 'assets/snowy-6.svg';
+          break;
+          case 'Clear':
+            this.weatherSrc='assets/cloudy-day-3.svg';
+            break;
+            case 'Smoke':
+              this.weatherSrc='assets/fog.png';
+              break;
+            case 'Haze':
+              this.weatherSrc='assets/fog.png';
+              break;
+          default:
+            this.weatherSrc= 'assets/sunny-1.svg'
+    }
+  }
+
+  onAdd(){
+    console.log('on add');
+    this.dataToPass={
+      cityname:this.cityName,
+      weatherdesc:this.weatherDesc,
+      temp:this.temp,
+      localdate:this.localDate
+    }
+    this.dialogref.close(this.cityName)
   }
 }
